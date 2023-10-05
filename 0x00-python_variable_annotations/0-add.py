b70bbcac3f51b5654709fc7c61299f4d8162efca
Luis Saiete <luistsaiete@gmail.com>
2023-10-05 13:40:09 +0200

    initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a simple module and it only has
+one function called add
+"""
+
+def add(a: float, b: float) -> float :
+    """takes a float a and a float b as arguments and
+    returns their sum as a float.
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    return a + b
