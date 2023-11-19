fa8da51d7717e83cb95a0c4cad59b4e3356e34d1
Luis Saiete <luistsaiete@gmail.com>
2023-11-19 10:30:05 +0200

    Fix task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,11 +20,11 @@ class TestAccessNestedMap(unittest.TestCase):
         unittest: unittest module
     """
 
-    @parameterized.expand[
+    @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-    ]
+    ])
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected_result: Union[Mapping, int]):
         """test that the method utils.access_nested_map returns what it is supposed to.
 
